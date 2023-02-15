d9e1cac3ea7f19888148641fe2507ed0f8b2d585
刘永华 <liuyonghua@ganweisoft.com>
2023-02-15 18:20:48 +0800

    feat:learn uniapp init

--- uni-preset-vue-vite-ts/vite.config.ts
+++ uni-preset-vue-vite-ts/vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
-import { resolve } from 'path'
-const srcPath = resolve(__dirname, 'src')
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [uni()],
-  resolve: {
-    alias: {
-      '@/': `${srcPath}/`
-    }
-  }
 });
